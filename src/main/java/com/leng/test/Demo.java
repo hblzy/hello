fd93cbf09bbef95bb367870489ea047d399bb4df
hblzy <54087585+hblzy@users.noreply.github.com>
2020-04-25 17:17:20 +0800

    test

---
+++ src/main/java/com/leng/test/Demo.java
@@ -0,0 +1,12 @@
+package com.leng.test;
+
+/**
+ * @Author:leng
+ * @Date:2020/4/25
+ * @Time:17:11
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
